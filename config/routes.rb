24e92a3473f0ee8d5d0945e05af1e3733c5a1413
easaka <sakakwame2002@gmail.com>
2022-06-10 17:55:33 +0000

    Working on functionality without styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
+  resources :post
   devise_for :funders
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
